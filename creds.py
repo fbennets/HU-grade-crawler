8c369587bc8bdf5e22de4dfb43cc8bc366000be0
Finn Schädlich <35722762+fbennets@users.noreply.github.com>
2020-03-31 13:15:34 +0200

    Add files via upload

---
+++ creds.py
@@ -0,0 +1,8 @@
+user = '' # Replace with your agnes.hu-berlin.de login name
+password = '' # Replace with your agnes.hu-berlin.de login name
+
+account = '' # Replace with your Twilio Account SID
+auth = '' # Twilio Authentification Token
+
+number = '' # Replace with your own mobile number where you want to
+# receive the SMS in international format e.g. '+49123456789'
